7533292d84fc4769e87b57cde6b924e05ae469ab
puan0601 <anton.pugach@gmail.com>
2017-02-10 08:13:36 -0800

    got create to work and display properly

--- models/book.js
+++ models/book.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var BookSchema = new Schema({
   title: String,
-  author: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'Author'
+  },
   image: String,
   releaseDate: String
 });
--- server.js
+++ server.js
@@ -74,9 +74,11 @@ app.get('/', function (req, res) {
 // get all books
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  db.Book.find(function(err, books) {
-    if (err) {return console.log(`index error: ${err}`);}
-    res.json(books);
+  db.Book.find()
+    .populate('author')
+    .exec(function(err, books) {
+      if (err) {return console.log(`index error: ${err}`);}
+      res.json(books);
   });
 });
 
@@ -93,12 +95,20 @@ app.get('/api/books/:id', function (req, res) {
 // create new book
 app.post('/api/books', function (req, res) {
   // create new book with form data (`req.body`)
-  var newBook = new db.Book(req.body);
+  var newBook = new db.Book({
+    title: req.body.title,
+    image: req.body.image,
+    releaseDate: req.body.releaseDate,
+  });
 
+  //will only add an author to a book if the author already exists
+  db.Author.findOne({name: req.body.author}, function(err, author) {
+    newBook.author = author;
   //save new book in db
-  newBook.save(function(err, book) {
-    if (err) {return console.log(`could not create`);}
-    res.json(book);
+    newBook.save(function(err, book) {
+      if (err) {return console.log(`could not create`);}
+      res.json(book);
+    });
   });
 });
 
@@ -118,6 +128,7 @@ app.put('/api/books/:id', function(req,res){
     //save updated book in db
     found.save(function(err, saved) {
       if (err) {console.log(`that was not saved properly`);}
+      console.log(saved);
       res.json(saved);
     });
 
